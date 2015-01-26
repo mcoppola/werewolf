deb654218c1aab15d61d14bb5bebe3fc6e5c88b0
mcoppola <mcoppola832@gmail.com>
2015-01-26 18:33:41 -0500

    getting direct message room id example

---
+++ README.md
@@ -0,0 +1,29 @@
+API
+-----------------------------
+
+
+ww new-game
+	- get all the people in the channel
+	- dm users, ask if they want to play
+	- 
+
+
+GAME
+-----------------------------
+
+day
+	nominate people to be lynched
+		3 people / factor or player count
+	vote on nominations
+		for each nomination, each player gives "yay"/"nay"/"refuse"
+	person with the most votes is dead
+		kick them from channel
+
+night
+	werewolves can communicate privatly
+	ww kill <user> 
+
+day 
+	killing is announced
+		user is kicked
+	(day)
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var Slack = require('slack-client');
+var Slack = require('slack-client'),
+	Message = require('./node_modules/slack-client/src/message'),
+	request = require('request');
 
 var token = 'xoxb-3522717080-Qe1d7lUKJWF8PYw5hYNmOuU6',
     autoReconnect = true,
@@ -6,6 +8,26 @@ var token = 'xoxb-3522717080-Qe1d7lUKJWF8PYw5hYNmOuU6',
     modId = "<@U03FCM32C>";
 
 var slack = new Slack(token, autoReconnect, autoMark);
+
+
+var getDMChannelFromUser = function(userId, callback) {
+	request.get({ 
+		uri: 'https://slack.com/api/im.open',
+		qs: {
+			token: token,
+			user: userId
+		}
+	}, function(e, res){
+		if (!e) {
+			return callback(JSON.parse(res.body).channel.id);
+		} else {
+			console.log(JSON.parse(e));
+			return null;
+		}
+		
+	});
+}
+
 
 slack.on('open', function() {
 
@@ -28,6 +50,7 @@ slack.on('open', function() {
 
 	console.log('Running werewolf game...');
 
+
 });
 
 slack.on('message', function(message) {
@@ -36,7 +59,7 @@ slack.on('message', function(message) {
 	    channel = slack.getChannelGroupOrDMByID(message.channel),
 	    user = slack.getUserByID(message.user),
 	    time = message.ts,
-	    text = message.text;
+	    text = message.text || '';
 
 	// Respond if pinged
 	if (type === 'message' && (text.indexOf(modId) >= 0 || text.toLowerCase().indexOf('ww') >= 0) && channel.name.toLowerCase() === 'werewolf') {
@@ -44,11 +67,21 @@ slack.on('message', function(message) {
 		console.log('*** ' + user.name + ' pinged the mod.');
 		channel.send(user.name + ' is a werewolf.');
 	}
+
+	// send a dm to that user
+	getDMChannelFromUser(message.user, function(dm) {
+
+		var m = new Message(channel._client, { channel: dm, text: 'this is a direct message!'});
+		channel._client._send(m);
+
+	});
+
 });
 
 slack.on('error', function(error) {
 
 	console.error('Error: %s', error);
 });
+
 
 slack.login();
